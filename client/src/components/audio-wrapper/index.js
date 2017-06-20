f522a6077f452fa719066eee0f20d0566b87f210
wshuang6 <seraph617@gmail.com>
2017-06-20 16:31:30 -0400

    Kick now works

---
+++ client/src/components/audio-wrapper/index.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { setIsPlaying, setNotPlaying, sendRecording, startRecording, stopRecording, requestToRecord } from './actions'
+import './index.css'
+
+class AudioWrapper extends React.Component {
+  // componentWillMount() {
+  //   this.audioContext = new AudioContext()
+  //   let timerID = 0
+  // }
+
+  componentDidMount () {
+    this.play();
+  }
+
+  componentWillUnmount() {
+    this.stop();
+  }
+
+  play() {
+    this.props.dispatch(setIsPlaying(this.props.bpm, this.props.timeSignature))
+  }
+
+  stop() {
+    this.props.dispatch(setNotPlaying())
+  }
+
+  sendRecording () {
+    this.props.dispatch(sendRecording(this.props.room))
+  }
+
+  render() {
+    let roommates = [];
+    for (let user in this.props.roommates) {
+      if (this.props.roommates[user].displayName) {
+        let thatsYou = (this.props.displayName === this.props.roommates[user].displayName) ? 
+          "(that's you!)" :
+          null;
+        roommates.push(<li key={user}>{this.props.roommates[user].displayName} {thatsYou}</li>)
+      }
+    }
+
+    let sendRecording = (this.props.enableSendRecording) ? 
+      (<button onClick={() => this.sendRecording()}>send recording</button>) :
+      null;
+
+    return (
+      <div>
+        {sendRecording}
+        <button onClick={() => this.props.dispatch(requestToRecord())}>req record</button>
+        <button onClick={() => this.props.dispatch(startRecording())}>start recording</button>
+        <button onClick={() => this.props.dispatch(stopRecording())}>stop recording</button>
+        <div className="play-button" onClick={() => this.play()}> > </div>
+        <div className="stop-button" onClick={() => this.stop()}> > </div>
+        <p>{this.props.recordingMessage}</p>
+        <p>Roommates</p>
+        <ul>{roommates}</ul>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = function (state, props) {
+  return {
+    isPlaying: state.audioWrapper.isPlaying,
+    bpm: state.audioWrapper.bpm,
+    timeSignature: state.audioWrapper.timeSignature,
+    room: state.socketWrapper.room,
+    displayName: state.socketWrapper.displayName,
+    roommates: state.audioWrapper.roommates,
+    recording: state.audioWrapper.recording,
+    recordingMessage: state.audioWrapper.recordingMessage,
+    enableSendRecording: state.audioWrapper.enableSendRecording
+  }
+}
+
+export default connect(mapStateToProps)(AudioWrapper)
---
+++ client/src/components/audio-wrapper/reducers.js
@@ -0,0 +1,52 @@
+import { SET_IS_PLAYING, SET_NOT_PLAYING, RECEIVE_RECORDING, START_RECORDING, STOP_RECORDING, UPDATE_RECORDING_MESSAGE, ENABLE_SEND_RECORDING } from "./actions"
+
+const initialState = {
+    isPlaying: false,
+    bpm: 60,
+    timeSignature: 4,
+    recording: false,
+    recordingMessage: 'Not recording',
+    enableSendRecording: false
+}
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case SET_IS_PLAYING:
+            return {
+                ...state,
+                isPlaying: true
+            }
+        case SET_NOT_PLAYING:
+            return {
+                ...state,
+                isPlaying: false
+            }
+        case START_RECORDING:
+            return {
+                ...state,
+                recording: true
+            }
+        case STOP_RECORDING: 
+            return {
+                ...state,
+                recording: false
+            }
+        case RECEIVE_RECORDING: 
+            return {
+                ...state,
+                roommates: action.roommates
+            }
+        case UPDATE_RECORDING_MESSAGE:
+            return {
+                ...state,
+                recordingMessage: action.recordingMessage
+            }
+        case ENABLE_SEND_RECORDING: 
+            return {
+                ...state,
+                enableSendRecording: action.enableSendRecording
+            }
+        default:
+            return state;
+    }
+}
